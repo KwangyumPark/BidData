9e42109c6629d425ab136108dab8969bcf1dccce
Kwangyum Park <kwangyum.park@gmail.com>
2020-12-21 22:29:31 +0900

    20-12-21

---
+++ Source/1.Java_source/20.12.21_GUI/com/lec/scissor_rock_paper_game/TestMain.java
@@ -0,0 +1,12 @@
+package com.lec.scissor_rock_paper_game;
+
+public class TestMain {
+		
+	public static void main(String[] args) {
+		
+		new ScissorRockPaper("가위, 바위, 보 게임");
+	
+	}// main
+
+	
+}// class
